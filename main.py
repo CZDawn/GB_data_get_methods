0aa14d69f047fd16706fa141121b482a8716c7fc
codezerodawn <codezerodawn@gmail.com>
2021-08-31 01:38:03 +0300

    done 1-2 points of home task

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import json
+from pprint import pprint
 from pymongo import MongoClient
 
 client = MongoClient('127.0.0.1', 27017)
 db = client['vacancies_db']
 collection = db.hh_vacansies
 
-with open('vacansies_info.json', 'r', encoding='UTF-8') as file:
-    data = json.load(file)
-    print(data)
+
+# Добавляет данные в базу из файла json
+def pushDataToDataBase(data: list):
+    for el in data:
+        for item in el:
+            collection.insert_one(item)
+
+def fillDataBaseByData():
+    with open('vacansies_info.json', 'r', encoding='UTF-8') as file:
+        data = json.load(file)
+        pushDataToDataBase(data)
+    print('Done! Data Base is filled by data from json file.')
+
+fillDataBaseByData()
+
+
+# Ищет все записи в базе у которых зарплата от 120000 до 200000
+def findVacansiesByCondition():
+    for el in collection.find({'$and': [
+                              {'salary_info.min_salary': {'$gt': 120000}},
+                              {'salary_info.max_salary': {'$lt': 200000}},
+                              {'salary_info.salary_currency': 'руб.'}]}):
+        pprint(el)
+
+findVacansiesByCondition()
 
